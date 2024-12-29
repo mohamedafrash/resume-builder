cdfc2f50be177f75d4fb8ef84005457f254eedb6
Mohamed Afrash <afrazzz51@gmail.com>
2024-12-29 11:56:36 +0000

    Initial commit: Setup Vite React TS with Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Hello Tailwind CSS!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
